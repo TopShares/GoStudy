c7e7f226fda81af5f4e827c9098699a8256b728d
root <1255877369@qq.com>
2019-02-10 08:40:08 +0800

    crawler

--- Crawler/main.go
+++ Crawler/main.go
@@ -1,1 +1,26 @@
 package Crawler
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, err := http.Get("http://www.zhenai.com/zhenghun")
+	if err != nil{
+		panic(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error:status code", resp.StatusCode)
+		return
+	}
+
+	all, err := ioutil.ReadAll(resp.Body)
+	if err != nil{
+		panic(err)
+	}
+	fmt.Printf("%s\n",all)
+
+}
