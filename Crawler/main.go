fdba18995dcf90937d8a205ade9273a7b672af9e
TopShare <1255877369@qq.com>
2019-02-16 18:31:35 +0800

    normal update

--- Crawler/main.go
+++ Crawler/main.go
@@ -9,9 +9,17 @@ import (
 const URL  = "http://www.zhenai.com/zhenghun"
 func main() {
 	//engine.SimpleEngine{}.Run(engine.Request{
-	e :=engine.ConcurrentEngine{Scheduler: &scheduler.QueuedScheduler{},WorkerCount:100,}
+	e :=engine.ConcurrentEngine{
+		Scheduler: &scheduler.QueuedScheduler{},
+		WorkerCount:100,
+		//ItemChan: persist.ItemSaver(),
+	}
+	//e.Run(engine.Request{
+	//	Url:URL,
+	//	Parsefunc: parser.ParseCityList,
+	//})
 	e.Run(engine.Request{
-		Url:URL,
-		Parsefunc: parser.ParseCityList,
+		Url: "http://www.zhenai.com/zhenghun/shanghai",
+		Parsefunc: parser.ParseCity,
 	})
 }
--- Crawler/zhenai/parser/city.go
+++ Crawler/zhenai/parser/city.go
@@ -5,12 +5,14 @@ import (
 	"regexp"
 )
 
-const cityRe  = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
+var (
+	profileRe  = regexp.MustCompile(`<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`)
+	cityUrlRe = regexp.MustCompile(`"href=(http://www.zhenai.com/zhenghun/[^"]+)"`)
+)
 
 func ParseCity(contents []byte) engine.ParseResult{
 
-	re:= regexp.MustCompile(cityRe)
-	matches := re.FindAllSubmatch(contents, -1)
+	matches := profileRe.FindAllSubmatch(contents, -1)
 
 	//fmt.Println(string(contents))
 	//fmt.Println(len(matches))
@@ -18,11 +20,20 @@ func ParseCity(contents []byte) engine.ParseResult{
 
 	result := engine.ParseResult{}
 	for _,m := range matches{
-		result.Items = append(result.Items, "User" + string(m[2]))
+		//result.Items = append(result.Items, "User" + string(m[2]))
 		result.Requests = append(result.Requests, engine.Request{
 			Url: string(m[1]),
 			Parsefunc: ParseProfile,
 		})
+	}
+
+	matches = cityUrlRe.FindAllSubmatch(contents,-1)
+	for _,m := range matches{
+		result.Requests= append(result.Requests,
+			engine.Request{
+				Url:string(m[1]),
+				Parsefunc:ParseCity,
+			})
 	}
 	return result
 }
